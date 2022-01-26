40f5c61c0db0fe1e879a1f63e022f68b27e8c3df
nwltc <98357821+nwltc@users.noreply.github.com>
2022-01-25 20:47:37 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: false,
-    eslint: {
+  reactStrictMode: true,
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-  }
-}
+  },
 
+}
